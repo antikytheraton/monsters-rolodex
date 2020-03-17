44722e739ab9decab973e79fec14a0b1b76397a2
Aaron <aaron@justo.mx>
2020-03-17 17:30:54 -0600

    Updates

---
+++ precache-manifest.990cd9a224b86933afe42bf27da0b58c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63dbb42a99a4c13c115e0171d02c005b",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "b28e9e899d63ac1ff8f3",
+    "url": "/mosters-rolodex/static/css/main.341402e2.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/mosters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mosters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b28e9e899d63ac1ff8f3",
+    "url": "/mosters-rolodex/static/js/main.dc158b90.chunk.js"
+  },
+  {
+    "revision": "2d731fc608c60f6f245d",
+    "url": "/mosters-rolodex/static/js/runtime-main.775f834d.js"
+  }
+]);
